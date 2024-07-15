0cb40faa68a76f72f0f390a6431968430a090e79
Neema <neema@MacBook-Air-de-Neema.local>
2024-07-15 15:39:50 +0200

    changing audios

--- screens/Main/index.js
+++ screens/Main/index.js
@@ -164,7 +164,7 @@ const Main = ({ navigation }) => {
           >
             <View style={[styles.imageContainer, styles.shadowProp]}>
               <Image
-                source={require("../../src/images/cancons2.jpg")}
+                source={require("../../src/images/canconss.jpg")}
                 style={styles.image}
               />
             </View>
